5cf05edea5d79a1708a67f267971507400506154
Xavier <xavier.astor.fontaine@gmail.com>
2018-07-05 14:22:36 +0000

    Deployment - Step 0 - Split settings into base / local / prod

---
+++ djangoapp/settings/local.py
@@ -0,0 +1,27 @@
+from .base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['project-django-pangur.c9users.io']
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_URL       = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT      = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL        = '/media/'
+MEDIA_ROOT       = os.path.join(BASE_DIR, 'media')
