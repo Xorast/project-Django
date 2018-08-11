ac70de8585679b50895d872794939df820de1ef3
Xavier <xavier.astor.fontaine@gmail.com>
2018-08-11 17:40:06 +0000

    Deployment fix

--- djangoapp/settings/prod.py
+++ djangoapp/settings/prod.py
@@ -23,10 +23,10 @@ AWS_SECRET_ACCESS_KEY       = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_HOST                 = 's3-eu-west-1.amazonaws.com'
 AWS_S3_CUSTOM_DOMAIN        = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATIC_URL                  = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 STATICFILES_DIRS            = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_LOCATION        = 'static'
 STATICFILES_STORAGE         = 'custom_storages.StaticStorage'
+STATIC_URL                  = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
 MEDIAFILES_LOCATION         = 'media'
 MEDIA_URL                   = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
