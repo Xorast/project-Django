2ca94c530eb0e4d24254b5019512fba4a40a659a
Xavier <xavier.astor.fontaine@gmail.com>
2018-08-13 00:09:00 +0000

    Basics of the feature enabling the creating of News from the front-end.

--- news/urls.py
+++ news/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls.static    import static
 from django.views.static        import serve
 from django.conf                import settings
 from django.urls                import path
-from .views                     import get_news_list
+from .views                     import get_news_list, create_news, news_details, edit_news
 
 
 urlpatterns = [
     path('', get_news_list, name='news_list'),
+    path('new/', create_news, name='create_news'),
+    path('<pk>/', news_details, name='news_details'),
+    path('<pk>/edit', edit_news, name='edit_news'),
     path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT})
 ]
