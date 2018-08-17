81ee45486f6de901f2bf968949f0e6a4b48005b0
Xavier <xavier.astor.fontaine@gmail.com>
2018-08-17 01:39:20 +0000

    Deployment fix

--- views/views.py
+++ views/views.py
@@ -6,13 +6,12 @@ from news.models            import News
 
 def get_home_page(request):
     
-    # last_news    = News.objects.order_by('-published_date')[:3]
-    # activities   = get_object_or_404(Event_Type, event_type="ACTIVITY")
-    # courses      = get_object_or_404(Event_Type, event_type="TRAININGCOURSE")
-    # events       = get_object_or_404(Event_Type, event_type="EVENT")
+    last_news    = News.objects.order_by('-published_date')[:3]
+    activities   = get_object_or_404(Event_Type, event_type="ACTIVITY")
+    courses      = get_object_or_404(Event_Type, event_type="TRAININGCOURSE")
+    events       = get_object_or_404(Event_Type, event_type="EVENT")
     
-    # return render(request, "views/index.html", {'last_news': last_news, 'activities': activities, 'courses': courses, 'events': events})
-    return render(request, "views/deployment.html")
+    return render(request, "views/index.html", {'last_news': last_news, 'activities': activities, 'courses': courses, 'events': events})
 
 
 
@@ -36,8 +35,6 @@ def get_host(request, id):
     return render(request, "views/host.html", {'host':host} )
 
 
-# def get_admin_panel(request):
-#     return redirect("/admin")
-    
-    
-    
+
+def get_admin_panel(request):
+    return redirect("/admin")
