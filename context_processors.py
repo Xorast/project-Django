f322ad0d3fcf5f6ab1c4603ba7457b6ee666e8a4
Xavier <xavier.astor.fontaine@gmail.com>
2018-08-17 02:25:43 +0000

    Navbar alpha order

--- context_processors.py
+++ context_processors.py
@@ -5,13 +5,13 @@ from activities.models      import Event, Event_Type, Event_Subtype
 def get_list_of_type_subtypes(request):
     
     event_type_ACTIVITY       = get_object_or_404(Event_Type, event_type='ACTIVITY')
-    event_subtypes_ACTIVITY   = Event_Subtype.objects.filter(Event__event_type = event_type_ACTIVITY.id).distinct()
+    event_subtypes_ACTIVITY   = Event_Subtype.objects.filter(Event__event_type = event_type_ACTIVITY.id).distinct().order_by('event_subtype')
     
     event_type_COURSE         = get_object_or_404(Event_Type, event_type='TRAININGCOURSE')
-    event_subtypes_COURSE     = Event_Subtype.objects.filter(Event__event_type = event_type_COURSE.id).distinct()
+    event_subtypes_COURSE     = Event_Subtype.objects.filter(Event__event_type = event_type_COURSE.id).distinct().order_by('event_subtype')
     
     event_type_EVENT          = get_object_or_404(Event_Type, event_type='EVENT')
-    event_subtypes_EVENT      = Event_Subtype.objects.filter(Event__event_type = event_type_EVENT.id).distinct()
+    event_subtypes_EVENT      = Event_Subtype.objects.filter(Event__event_type = event_type_EVENT.id).distinct().order_by('event_subtype')
     
     
     return { 'event_subtypes_ACTIVITY'  : event_subtypes_ACTIVITY, 
