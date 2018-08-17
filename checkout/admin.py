ba321f671e3c8c572dd2e51b5fc0674f13dc5330
Xavier <xavier.astor.fontaine@gmail.com>
2018-08-17 01:29:32 +0000

    Deployment fix

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,5 +1,5 @@
-# from django.contrib     import admin
-# from .models            import RegistrationOrder
+from django.contrib     import admin
+from .models            import RegistrationOrder
 
 
-# admin.site.register(RegistrationOrder)
+admin.site.register(RegistrationOrder)
