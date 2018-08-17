3c6a51e7990c7150e57546172ed5c83906c29edd
Xavier <xavier.astor.fontaine@gmail.com>
2018-08-17 16:00:11 +0000

    Admin panel - order by

--- activities/admin.py
+++ activities/admin.py
@@ -1,9 +1,16 @@
 from django.contrib     import admin
 from .models            import EventRegistration, Event, Event_Type, Event_Subtype, Age_Group,Level,Venue, Room, Period, Host, Weekday
 
+
 class AgeGroupAdmin(admin.ModelAdmin):  
     list_display    = ['age_group']
     ordering        = ('age_group',)
+
+
+class RoomAdmin(admin.ModelAdmin):  
+    list_display    = ['venue', 'room']
+    ordering        = ('venue','room')
+
 
 admin.site.register(EventRegistration)
 admin.site.register(Event)
@@ -12,7 +19,7 @@ admin.site.register(Event_Subtype)
 admin.site.register(Age_Group, AgeGroupAdmin)
 admin.site.register(Level)
 admin.site.register(Venue)
-admin.site.register(Room)
+admin.site.register(Room, RoomAdmin)
 admin.site.register(Period)
 admin.site.register(Host)
 admin.site.register(Weekday)
