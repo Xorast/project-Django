87b50acfca4390557568e56be0973fb80296afd1
Xavier <xavier.astor.fontaine@gmail.com>
2018-08-29 11:24:40 +0000

    Data input - Adjustment

--- events/views.py
+++ events/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts       import render, redirect, get_object_or_404, HttpResponse
 from .models                import Event_Family, Event
-
+from datetime               import datetime, timedelta
 
 
 
 
 def get_list_of_all_events(request):
     
-    events = Event.objects.order_by('date_first_day')
+    events_coming = Event.objects.filter(date_last_day__gte=datetime.now()).order_by('date_first_day')
+    events_past   = Event.objects.filter(date_last_day__lt=datetime.now()).order_by('date_first_day')
     
-    return render(request, "events/list_of_all_events.html", {'events':events})
+    return render(request, "events/list_of_all_events.html", {'events_coming':events_coming, 'events_past':events_past})
     
     
     
