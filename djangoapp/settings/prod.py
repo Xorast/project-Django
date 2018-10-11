29b07a4e0126ef755378ca18387391604eec8a6f
Xavier <xavier.astor.fontaine@gmail.com>
2018-10-11 19:55:13 +0000

    New web address added

--- djangoapp/settings/prod.py
+++ djangoapp/settings/prod.py
@@ -7,7 +7,7 @@ SECRET_KEY  = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mjcmauguiocarnon.herokuapp.com']
+ALLOWED_HOSTS = ['mjcmauguiocarnon.herokuapp.com', 'mjc-mauguio-carnon.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
