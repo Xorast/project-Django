2fbab327dc3969a98315d4ad7ac19f4b9ca4cca3
Xavier <xavier.astor.fontaine@gmail.com>
2018-10-24 15:04:57 +0000

    CI Testing implemented

--- accounts/tests_forms.py
+++ accounts/tests_forms.py
@@ -1,22 +1,25 @@
-from django.test    import TestCase
-from .forms         import UserRegistrationForm, ProfileRegistrationForm
+from django.test                    import TestCase
+from django.contrib.auth.models     import User
+from .forms                         import UserRegistrationForm, ProfileRegistrationForm
 
 
 class TestAccountsForms(TestCase):
     
-    def test_registration_form(self):
-        form=UserRegistrationForm({
-            'username': 'test',
-            'email':'test@gmail.com',
-            'password1' : 'password1234_*/',
-            'password2' : 'password1234_*/'
-        })
-        self.assertTrue(form.is_valid())
+    def setUp(self):
+        
+        self.userform     = UserRegistrationForm({'username':'testuser2568',
+                                        'email':'test@gmail.com',
+                                        'password1':'password1234_*/',
+                                        'password2':'password1234_*/'
+                                        })
+        self.user         = User.objects.create_user(username='testuser9999', password='password1234_*/', email='test9999@gmail.com')
+        self.profileform  = ProfileRegistrationForm({'user' : self.user, 
+                                                     'adherent_number': '1234', 
+                                                     'rate_coefficient':'1'})
     
-    # def test_profile_form(self):
-    #     form=ProfileRegistrationForm({
-    #         'user':'test',
-    #         'adherent_number': '1234567',
-    #         'rate_coefficient':'1',
-    #     })
-    #     self.assertTrue(form.is_valid())
+    def test_registration_form(self):
+        self.assertTrue(self.userform.is_valid())
+   
+   
+    def test_profile_form(self):
+      self.assertTrue(self.profileform.is_valid())
