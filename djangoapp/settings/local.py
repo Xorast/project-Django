bf21ea108254dbf30d2a94dba4fcf5d34e54ee94
Xavier <xavier.astor.fontaine@gmail.com>
2018-10-26 01:27:05 +0000

    Fix static back

--- djangoapp/settings/local.py
+++ djangoapp/settings/local.py
@@ -21,8 +21,7 @@ DATABASES = {
     }
 }
 
-# STATIC_URL       = '/static/'
-STATIC_URL       = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL       = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT      =  os.path.join(BASE_DIR, 'staticfiles')
 
