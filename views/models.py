0ed3ee49821042b03e64a83300fc4e268c73a89b
Xavier <xavier.astor.fontaine@gmail.com>
2019-01-09 21:46:57 +0000

    Carousel DB

--- views/models.py
+++ views/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Slides(models.Model):
     
-    slide_title      = models.CharField("Diapo - Titre", max_length=100, null=False, blank=False)
-    slide_subtitle   = models.CharField("Diapo - Sous titre", max_length=150, null=False, blank=False)
+    slide_title      = models.CharField("Diapo - Titre", max_length=100, null=True, blank=True)
+    slide_subtitle   = models.CharField("Diapo - Sous titre", max_length=150, null=True, blank=True)
     image            = models.ImageField("Diapo - Image", upload_to="images/carousel", blank=True, null=True)
     
     def __str__(self):
             return 'Diapositive %s' % (str(self.id))
         
     class Meta:
-        verbose_name = "1. Diapositives"
+        verbose_name = "Diapositive"
         verbose_name_plural = "Caroussel"
