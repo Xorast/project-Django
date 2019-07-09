adae4582b4053be392e20a9afa4e2f8ec601c2b3
pangur <pangur@tutanota.com>
2019-07-09 14:17:10 +0100

    Rework preparation

--- context_processors.py
+++ context_processors.py
@@ -1,75 +1,34 @@
-from activities.models      import Activity_Animation_Type, Elements_Type
-from workshops.models       import Workshop_Animation_Type
-from files.models           import Files_Pseudostatic
-
-
-
-
-
+from activities.models import Activity_Animation_Type, Elements_Type
+from workshops.models import Workshop_Animation_Type
+from files.models import Files_Pseudostatic
 
 
 def get_list_of_animation_types_activities(request):
-    
-   animations_types_activities = Activity_Animation_Type.objects.all().order_by('animation_type')
-
-   return {'animations_types_activities':animations_types_activities}
-
+    animations_types_activities = Activity_Animation_Type.objects.all().order_by('animation_type')
+    return {'animations_types_activities':animations_types_activities}
 
 
 def get_list_of_animation_types_workshops(request):
-    
-   animations_types_workshops = Workshop_Animation_Type.objects.all().order_by('animation_type')
+    animations_types_workshops = Workshop_Animation_Type.objects.all().order_by('animation_type')
+    return {'animations_types_workshops':animations_types_workshops}
+   
 
-   return {'animations_types_workshops':animations_types_workshops}
-   
-   
-   
 def get_elements_types(request):
-    
-    
+
     try:
         activity = Elements_Type.objects.get(element_type__exact="ACTIVITY")
         workshop = Elements_Type.objects.get(element_type__exact="WORKSHOP")
-        event    = Elements_Type.objects.get(element_type__exact="EVENT")
+        event = Elements_Type.objects.get(element_type__exact="EVENT")
     except:
         activity = "Your admin need to fix that! =)"
         workshop = "Your admin need to fix that! =)"
-        event    = "Your admin need to fix that! =)"
+        event = "Your admin need to fix that! =)"
     
-    return {'activity':activity, 'workshop':workshop, 'event':event}
+    return {'activity': activity,
+            'workshop': workshop,
+            'event': event}
     
-
 
 def get_documents(request):
-    
     documents = Files_Pseudostatic.objects.all()
-    
-    return  {'documents':documents}
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-# If context processor still required, do it by objects.filter instead of get_object_or_404
-
-# def get_list_of_type_subtypes(request):
-    
-#     event_type_ACTIVITY       = get_object_or_404(Event_Type, event_type='ACTIVITY')
-#     event_subtypes_ACTIVITY   = Event_Subtype.objects.filter(Event__event_type = event_type_ACTIVITY.id).distinct().order_by('event_subtype')
-    
-#     event_type_COURSE         = get_object_or_404(Event_Type, event_type='TRAININGCOURSE')
-#     event_subtypes_COURSE     = Event_Subtype.objects.filter(Event__event_type = event_type_COURSE.id).distinct().order_by('event_subtype')
-    
-#     event_type_EVENT          = get_object_or_404(Event_Type, event_type='EVENT')
-#     event_subtypes_EVENT      = Event_Subtype.objects.filter(Event__event_type = event_type_EVENT.id).distinct().order_by('event_subtype')
-    
-#     return { 'event_subtypes_ACTIVITY'  : event_subtypes_ACTIVITY, 
-#              'event_subtypes_COURSE'    : event_subtypes_COURSE, 
-#              'event_subtypes_EVENT'     : event_subtypes_EVENT }
-    
-    
+    return {'documents': documents}
