db768707a6c59d05c7e12f238b87154154e26e78
Xorast <xavier.astor.fontaine@gmail.com>
2019-09-15 19:40:25 +0100

    Local settings updated (local database)

--- djangoapp/settings/local.py
+++ djangoapp/settings/local.py
@@ -6,15 +6,21 @@ SECRET_KEY = 'luf*lin!+xg$hf2z-m*c9b)kaamk3xm$+xf@5@dxc3tw!t31mt'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['project-django-pangur.c9users.io']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'pangur',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
